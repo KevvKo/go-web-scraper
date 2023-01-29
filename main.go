deb81604c054624784e76761e4a20bc4406f72c6
k.klein <k.klein@fin-de-nb-0039.fritz.box>
2023-01-29 13:17:45 +0100

    update user agent

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ func main() {
 
 	//domain := colly.AllowedDomains("oxylabs.io")
 
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"),
 
+	)
 	c.OnHTML("body", func(e *colly.HTMLElement) {
-		fmt.Print("test")
 
 		links :=  e.ChildAttrs("a", "href")
 
